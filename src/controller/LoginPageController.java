172c00ef3be164e8f65f8df9d59e3de647cad087
esi <sunlines1398@gmail.com>
2023-06-05 13:48:27 +0430

    edited loginpage controller

--- src/controller/LoginPageController.java
+++ src/controller/LoginPageController.java
@@ -1,5 +1,23 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginPageController {
+
+    @FXML
+    PasswordField pwdField;
+    @FXML
+    TextField usernameField;
+    @FXML
+    Button signupBTN;
+    @FXML
+    Button signinBTN;
+
+    public void bia() {
+        System.out.println("loged in");
+    }
 
 }
